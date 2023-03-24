0c393fc8b1db7f001bb261c54392f86cf187d7e6
desi <deskanenova05@gmail.com>
2023-03-24 21:00:35 +0200

    Added medical review form

--- HospitalSystem/MedicalReview.Designer.cs
+++ HospitalSystem/MedicalReview.Designer.cs
@@ -28,12 +28,283 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MedicalReview));
+            panel1 = new Panel();
+            label11 = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            label5 = new Label();
+            textBox2 = new TextBox();
+            label6 = new Label();
+            label9 = new Label();
+            label7 = new Label();
+            maskedTextBox1 = new MaskedTextBox();
+            label10 = new Label();
+            maskedTextBox2 = new MaskedTextBox();
+            label8 = new Label();
+            comboBox1 = new ComboBox();
+            button1 = new Button();
+            button2 = new Button();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.PaleTurquoise;
+            panel1.Controls.Add(label11);
+            panel1.Location = new Point(-6, -9);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1053, 98);
+            panel1.TabIndex = 5;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label11.ForeColor = Color.White;
+            label11.Location = new Point(382, 36);
+            label11.Name = "label11";
+            label11.Size = new Size(253, 38);
+            label11.TabIndex = 0;
+            label11.Text = "MEDICAL REVIEW";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.MediumTurquoise;
+            label1.Location = new Point(-91, 86);
+            label1.Name = "label1";
+            label1.Size = new Size(1230, 25);
+            label1.TabIndex = 6;
+            label1.Text = "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.MediumTurquoise;
+            label2.Location = new Point(142, 138);
+            label2.Name = "label2";
+            label2.Size = new Size(125, 28);
+            label2.TabIndex = 7;
+            label2.Text = "DIAGNOSIS:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.ForeColor = Color.MediumTurquoise;
+            label4.Location = new Point(59, 166);
+            label4.Name = "label4";
+            label4.Size = new Size(291, 20);
+            label4.TabIndex = 11;
+            label4.Text = "-----------------------------------------------";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(59, 201);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(291, 191);
+            textBox1.TabIndex = 12;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ForeColor = Color.MediumTurquoise;
+            label3.Location = new Point(125, 410);
+            label3.Name = "label3";
+            label3.Size = new Size(156, 28);
+            label3.TabIndex = 13;
+            label3.Text = "PRESCRIPTION:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.ForeColor = Color.MediumTurquoise;
+            label5.Location = new Point(59, 438);
+            label5.Name = "label5";
+            label5.Size = new Size(291, 20);
+            label5.TabIndex = 14;
+            label5.Text = "-----------------------------------------------";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(59, 476);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(292, 191);
+            textBox2.TabIndex = 15;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.ForeColor = Color.MediumTurquoise;
+            label6.Location = new Point(598, 138);
+            label6.Name = "label6";
+            label6.Size = new Size(191, 28);
+            label6.TabIndex = 16;
+            label6.Text = "HOSPITALIZATION:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI Black", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label9.ForeColor = Color.MediumTurquoise;
+            label9.Location = new Point(536, 166);
+            label9.Name = "label9";
+            label9.Size = new Size(291, 20);
+            label9.TabIndex = 17;
+            label9.Text = "-----------------------------------------------";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.ForeColor = Color.MediumTurquoise;
+            label7.Location = new Point(418, 234);
+            label7.Name = "label7";
+            label7.Size = new Size(243, 25);
+            label7.TabIndex = 18;
+            label7.Text = "Date of hospital admission:";
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            maskedTextBox1.Location = new Point(690, 231);
+            maskedTextBox1.Mask = "00/00/0000";
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(215, 31);
+            maskedTextBox1.TabIndex = 19;
+            maskedTextBox1.ValidatingType = typeof(DateTime);
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label10.ForeColor = Color.MediumTurquoise;
+            label10.Location = new Point(421, 319);
+            label10.Name = "label10";
+            label10.Size = new Size(240, 25);
+            label10.TabIndex = 20;
+            label10.Text = "Date of hospital discharge:";
+            // 
+            // maskedTextBox2
+            // 
+            maskedTextBox2.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            maskedTextBox2.Location = new Point(690, 316);
+            maskedTextBox2.Mask = "00/00/0000";
+            maskedTextBox2.Name = "maskedTextBox2";
+            maskedTextBox2.Size = new Size(215, 31);
+            maskedTextBox2.TabIndex = 21;
+            maskedTextBox2.ValidatingType = typeof(DateTime);
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.ForeColor = Color.MediumTurquoise;
+            label8.Location = new Point(594, 395);
+            label8.Name = "label8";
+            label8.Size = new Size(67, 25);
+            label8.TabIndex = 22;
+            label8.Text = "Room:";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(690, 396);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(215, 28);
+            comboBox1.TabIndex = 23;
+            // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.MediumTurquoise;
+            button1.Location = new Point(434, 526);
+            button1.Name = "button1";
+            button1.Size = new Size(206, 88);
+            button1.TabIndex = 24;
+            button1.Text = "SUBMIT";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.ForeColor = Color.MediumTurquoise;
+            button2.Location = new Point(699, 526);
+            button2.Name = "button2";
+            button2.Size = new Size(206, 88);
+            button2.TabIndex = 25;
+            button2.Text = "BACK TO PROFILE";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // MedicalReview
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "PatientAccountView";
+            BackColor = Color.White;
+            ClientSize = new Size(1004, 690);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(comboBox1);
+            Controls.Add(label8);
+            Controls.Add(maskedTextBox2);
+            Controls.Add(label10);
+            Controls.Add(maskedTextBox1);
+            Controls.Add(label7);
+            Controls.Add(label9);
+            Controls.Add(label6);
+            Controls.Add(textBox2);
+            Controls.Add(label5);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(panel1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "MedicalReview";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "HMS | Medical Review";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Panel panel1;
+        private Label label11;
+        private Label label1;
+        private Label label2;
+        private Label label4;
+        private TextBox textBox1;
+        private Label label3;
+        private Label label5;
+        private TextBox textBox2;
+        private Label label6;
+        private Label label9;
+        private Label label7;
+        private MaskedTextBox maskedTextBox1;
+        private Label label10;
+        private MaskedTextBox maskedTextBox2;
+        private Label label8;
+        private ComboBox comboBox1;
+        private Button button1;
+        private Button button2;
     }
 }
