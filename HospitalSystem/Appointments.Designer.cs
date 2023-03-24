f82f473931e000181933b0e97e910d05fd0d3ba9
desi <deskanenova05@gmail.com>
2023-03-24 21:34:09 +0200

    Upgraded appointments form

--- HospitalSystem/Appointments.Designer.cs
+++ HospitalSystem/Appointments.Designer.cs
@@ -33,17 +33,13 @@
             panel1 = new Panel();
             label11 = new Label();
             label2 = new Label();
-            dataGridView1 = new DataGridView();
             textBox1 = new TextBox();
             maskedTextBox1 = new MaskedTextBox();
             label5 = new Label();
             label3 = new Label();
             button1 = new Button();
-            Speciality = new DataGridViewTextBoxColumn();
-            DoctorName = new DataGridViewTextBoxColumn();
-            Choose = new DataGridViewButtonColumn();
+            comboBox1 = new ComboBox();
             panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // label1
@@ -51,7 +47,7 @@
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
             label1.ForeColor = Color.MediumTurquoise;
-            label1.Location = new Point(406, 348);
+            label1.Location = new Point(135, 393);
             label1.Name = "label1";
             label1.Size = new Size(176, 31);
             label1.TabIndex = 0;
@@ -88,25 +84,12 @@
             label2.TabIndex = 5;
             label2.Text = "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------";
             // 
-            // dataGridView1
-            // 
-            dataGridView1.BackgroundColor = Color.PaleTurquoise;
-            dataGridView1.BorderStyle = BorderStyle.None;
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Speciality, DoctorName, Choose });
-            dataGridView1.Location = new Point(23, 394);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.RowTemplate.Height = 29;
-            dataGridView1.Size = new Size(951, 126);
-            dataGridView1.TabIndex = 6;
-            // 
             // textBox1
             // 
             textBox1.BorderStyle = BorderStyle.FixedSingle;
             textBox1.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
             textBox1.ForeColor = SystemColors.WindowFrame;
-            textBox1.Location = new Point(354, 186);
+            textBox1.Location = new Point(354, 189);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(307, 31);
             textBox1.TabIndex = 8;
@@ -118,7 +101,7 @@
             maskedTextBox1.BorderStyle = BorderStyle.FixedSingle;
             maskedTextBox1.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
             maskedTextBox1.ForeColor = SystemColors.WindowFrame;
-            maskedTextBox1.Location = new Point(354, 268);
+            maskedTextBox1.Location = new Point(354, 294);
             maskedTextBox1.Mask = "00/00/0000 90:00";
             maskedTextBox1.Name = "maskedTextBox1";
             maskedTextBox1.Size = new Size(307, 31);
@@ -131,7 +114,7 @@
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
             label5.ForeColor = Color.MediumTurquoise;
-            label5.Location = new Point(241, 265);
+            label5.Location = new Point(241, 291);
             label5.Name = "label5";
             label5.Size = new Size(70, 31);
             label5.TabIndex = 12;
@@ -153,38 +136,24 @@
             // button1
             // 
             button1.FlatStyle = FlatStyle.Flat;
-            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
             button1.ForeColor = Color.MediumTurquoise;
-            button1.Location = new Point(376, 567);
+            button1.Location = new Point(354, 516);
             button1.Name = "button1";
-            button1.Size = new Size(266, 65);
+            button1.Size = new Size(307, 65);
             button1.TabIndex = 14;
             button1.Text = "COMPLETE";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
-            // Speciality
+            // comboBox1
             // 
-            Speciality.HeaderText = "Speciality";
-            Speciality.MinimumWidth = 6;
-            Speciality.Name = "Speciality";
-            Speciality.Width = 300;
-            // 
-            // DoctorName
-            // 
-            DoctorName.HeaderText = "Doctor Name";
-            DoctorName.MinimumWidth = 6;
-            DoctorName.Name = "DoctorName";
-            DoctorName.Width = 300;
-            // 
-            // Choose
-            // 
-            Choose.HeaderText = "Choose";
-            Choose.MinimumWidth = 6;
-            Choose.Name = "Choose";
-            Choose.Text = "Choose";
-            Choose.UseColumnTextForButtonValue = true;
-            Choose.Width = 300;
+            comboBox1.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(354, 395);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(307, 33);
+            comboBox1.TabIndex = 15;
             // 
             // Appointments
             // 
@@ -193,12 +162,12 @@
             BackColor = Color.White;
             BackgroundImageLayout = ImageLayout.None;
             ClientSize = new Size(1004, 690);
+            Controls.Add(comboBox1);
             Controls.Add(button1);
             Controls.Add(label3);
             Controls.Add(label5);
             Controls.Add(maskedTextBox1);
             Controls.Add(textBox1);
-            Controls.Add(dataGridView1);
             Controls.Add(label2);
             Controls.Add(panel1);
             Controls.Add(label1);
@@ -210,7 +179,6 @@
             Text = "HMS | Appointments";
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -221,14 +189,11 @@
         private Panel panel1;
         private Label label11;
         private Label label2;
-        private DataGridView dataGridView1;
         private TextBox textBox1;
         private MaskedTextBox maskedTextBox1;
         private Label label5;
         private Label label3;
         private Button button1;
-        private DataGridViewTextBoxColumn Speciality;
-        private DataGridViewTextBoxColumn DoctorName;
-        private DataGridViewButtonColumn Choose;
+        private ComboBox comboBox1;
     }
 }
